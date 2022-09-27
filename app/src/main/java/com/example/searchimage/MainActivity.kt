07711cc455f5282cf5787588d503a61aaeede0e3
jung-dayoon <jung.dayoon@worksmobile.com>
2022-09-27 21:58:31 +0900

    initial commit

---
+++ app/src/main/java/com/example/searchimage/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.searchimage
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.Text
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            Text("Hello World")
+        }
+    }
+}
