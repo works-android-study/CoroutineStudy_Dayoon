355760bad9c5f8c0c3c7cfdc1a1e200f8c483cae
jung-dayoon <jung.dayoon@worksmobile.com>
2022-10-12 18:28:53 +0900

    이미지 리스트 ui 작업

--- app/src/main/java/com/example/searchimage/MainActivity.kt
+++ app/src/main/java/com/example/searchimage/MainActivity.kt
@@ -1,16 +1,62 @@
 package com.example.searchimage
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.Text
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.skydoves.landscapist.glide.GlideImage
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    private val viewModel: SearchImageViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        initViewModel()
         setContent {
-            Text("Hello World")
+            ImageList()
         }
+    }
+
+    @Preview
+    @Composable
+    fun ImageList() {
+        val imageLinkList = viewModel.imageLinkList
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(minSize = 128.dp),
+            contentPadding = PaddingValues(8.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp),
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
+            content = {
+            items(imageLinkList) { link ->
+                if (link != null) {
+                    ImageItem(link)
+                }
+            }
+        })
+    }
+
+    @Composable
+    fun ImageItem(link: String) {
+        GlideImage(
+            imageModel = link,
+            modifier = Modifier.size(128.dp)
+        )
+    }
+
+    private fun initViewModel() {
+        viewModel.getSearchImage()
     }
 }
