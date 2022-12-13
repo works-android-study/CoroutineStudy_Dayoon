3c7f04ef838890dabd38cd3182797f84b9afad1a
jung-dayoon <jung.dayoon@worksmobile.com>
2022-12-13 23:32:12 +0900

    SearchImageViewModelTest 추가

---
+++ app/src/test/java/com/example/searchimage/SearchImageViewModelTest.kt
@@ -0,0 +1,57 @@
+package com.example.searchimage
+
+import com.example.searchimage.db.BookmarkLocalApi
+import com.example.searchimage.model.dto.Item
+import com.example.searchimage.model.entity.Bookmark
+import com.example.searchimage.network.api.DownloadApiClient
+import com.example.searchimage.network.api.SearchApiClient
+import com.example.searchimage.ui.SearchImageViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.spy
+import org.mockito.MockitoAnnotations
+
+@ExperimentalCoroutinesApi
+class SearchImageViewModelTest {
+    @get:Rule
+    var rule: TestRule = TestDispatcherRule()
+
+    lateinit var viewModel: SearchImageViewModel
+
+    @Mock
+    lateinit var searchApiClient: SearchApiClient
+    @Mock lateinit var downloadApiClient: DownloadApiClient
+    @Mock lateinit var bookmarkLocalApi: BookmarkLocalApi
+    @Mock lateinit var bookmark: Bookmark
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        viewModel = spy(SearchImageViewModel(
+            searchApiClient, downloadApiClient, bookmarkLocalApi
+        ))
+    }
+
+    @Test
+    fun getBookmarkListTest() = runTest {
+        val flow: Flow<List<Bookmark>> = flow {
+            emit(listOf(bookmark))
+        }
+        `when`(bookmarkLocalApi.getBookmarkList()).thenReturn(flow)
+
+        viewModel.initBookmarkList()
+        val result = viewModel.bookmarkListFlow.first()
+        assertEquals(listOf(bookmark), result)
+    }
+
+}
